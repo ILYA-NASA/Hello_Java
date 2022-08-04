be5a54152ddfb7d712874f5dd624a7916b44ae90
Илья Насалевич <il.nasa@outlook.com>
2022-08-04 09:56:27 +0300

    added example

--- lecture1/program.java
+++ lecture1/program.java
@@ -1,6 +1,11 @@
 package lecture1;
 public class program {
+    // public static void main(String[] args) {
+    //     System.out.println("hi java!");
+    // }
     public static void main(String[] args) {
-        System.out.println("hi java!");   
+        int i = 123;
+        System.out.println(Integer.MIN_VALUE); // обертка Integer
+        System.out.println(Integer.MAX_VALUE); // обертка Integer
     }
 }
