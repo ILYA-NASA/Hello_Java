c00e1dc35f70c3ac18718ced4f178c5a4a0b76fc
Илья Насалевич <il.nasa@outlook.com>
2022-08-10 11:30:55 +0300

    exercises 3 and 4 from home work 1 is done

--- home1/Program.java
+++ home1/Program.java
@@ -1,5 +1,9 @@
 package home1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Program {
 
     // 1 Выбросить случайное целое число и сохранить в i
@@ -15,30 +19,69 @@ public class Program {
     }
 
     // 3 Найти все кратные n числа большие i и сохранить в массив m1
-    public static int[] arrayMult(int i, int n, int max) {
-        int arrLen = ((int) (max / n)) - ((int) (i / n)); //определяю длину массива
-        int[] arr = new int[arrLen];
+    public static ArrayList<Integer> arrayMult(int i, int n, int max) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
         for (int j = i + 1; j <= max; j++) {
-            if (j % n == 0){
-                return arr[j]; //осталось вспомнить как заполнять и выводить массив в консоль 
+            if (j % n == 0) {
+                list.add(j);
             }
+        }
+        return list;
+    }
+
+    // 4 Найти все некратные n числа меньшие i и сохранить в массив m2
+    public static ArrayList<Integer> arrayNotMult(int i, int n, int min) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int j = i - 1; j >= min; j--) {
+            if (j % n != 0) {
+                list.add(j);
+            }
+        }
+        return list;
+    }
+
+    // 5 Сохранить оба массива в файлы с именами m1 и m2 соответственно.
+    public static void saveArrayM1(ArrayList<Integer> arr) {
+        try {
+            FileWriter writer = new FileWriter("m1.txt");
+            for (Integer c : arr) {
+                writer.write(c + "\t");
+                // writer.write(System.getProperty("line.separator")); // построчная запись
+            }
+            writer.close();
+            ;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            ;
         }
     }
 
+    public static void saveArrayM2(ArrayList<Integer> arr) {
+        try {
+            FileWriter writer = new FileWriter("m2.txt");
+            for (Integer c : arr) {
+                writer.write(c + "\t");
+                // writer.write(System.getProperty("line.separator")); // построчная запись
+            }
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         int min = 1, max = 10_000;
         int i = randomNum(min, max);
         System.out.println("integer between " + min + " and " + max + ": i = " + i);
         int n = seniorBit(i);
         System.out.println("number of the senior bit from i: n = " + n);
-        int[] m1 = arrayMult(i, n, max);
-        System.out.print("array multiple n > i: " + m1 );
+        ArrayList<Integer> m1 = arrayMult(i, n, max);
+        // System.out.print("array values multiple n > i: " + m1);
+        ArrayList<Integer> m2 = arrayNotMult(i, n, min);
+        // System.out.print("array values not multiple n < i: " + m2);
+        saveArrayM1(m1);
+        saveArrayM2(m2);
     }
-
 }
-
-// 4 Найти все некратные n числа меньшие i и сохранить в массив m2
-// 5 Сохранить оба массива в файлы с именами m1 и m2 соответственно.
 
 // Пункты реализовать в методе main
 // *Пункты реализовать в разных методах
