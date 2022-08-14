cd41d19ea6b3e6f9186448beca9673ef0384e90f
Илья Насалевич <il.nasa@outlook.com>
2022-08-14 15:53:53 +0300

    exercises 4,5 and 6 from homework 2 is done

--- home2/Program.java
+++ home2/Program.java
@@ -25,16 +25,53 @@ public class Program {
     public static boolean searchPalindrom(String firstStr, String secondStr) {
         return firstStr.equals(new StringBuilder(secondStr).reverse().toString());
     }
-    
+
     // 3 *Напишите программу на Java, чтобы перевернуть строку с помощью рекурсии.
     public static String reverseStr(String str) {
         if (str.length() <= 1) {
-           return str;
+            return str;
         }
         return reverseStr(str.substring(1)) + str.charAt(0);
     }
 
-    public static void main(String[] args) {
+    // 4 Дано два числа, например 3 и 56, необходимо составить следующие строки:
+    // 3 + 56 = 59
+    // 3 – 56 = -53
+    // 3 * 56 = 168
+    // Используем метод StringBuilder.append().
+    public static String compilerMathToStr(int num1, int num2, char sign) {
+        StringBuilder mathTask = new StringBuilder();
+        mathTask.append(num1).append(" ").append(sign).append(" ").append(num2).append(" = ");
+        switch (sign) {
+            case '+':
+                mathTask.append(num1 + num2);
+                break;
+            case '-':
+                mathTask.append(num1 - num2);
+                break;
+            case '*':
+                mathTask.append(num1 * num2);
+                break;
+            }
+            return mathTask.toString();
+        }
+        
+        // 5 Замените символ “=” на слово “равно”. Используйте методы
+        // StringBuilder.insert(),
+        // StringBuilder.deleteCharAt().
+        public static String replacEqual(String mathTask) {
+            StringBuilder task = new StringBuilder(mathTask);
+            return task.deleteCharAt(7).insert(7, "equal").toString();
+        }
+        
+        // 6 *Замените символ “=” на слово “равно”. Используйте методы
+        // StringBuilder.replace().
+        public static String nextReplacEquel(String mathTask) {
+            StringBuilder task = new StringBuilder(mathTask);
+            return task.replace(7, 8, "equel").toString();
+        }
+
+        public static void main(String[] args) {
         Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите первую строку: ");
         String str1 = iScanner.nextLine();
@@ -43,31 +80,39 @@ public class Program {
         iScanner.close();
 
         // 1
-        System.out.println("Ниже все наименьшие окна первой строки, содержащие все символы второй строки");
+        System.out.println("1.\nНиже все наименьшие окна первой строки, содержащие все символы второй строки (при их наличии): ");
         minWindow(str1, str2);
-        System.out.println();
 
         // 2
+        System.out.println();
         if (searchPalindrom(str1, str2)) {
-            System.out.println("Введенные вами строки являются вращением друг друга");
+            System.out.println("2.\nВведенные вами строки являются вращением друг друга");
         } else
-            System.out.println("Введенные вами строки НЕ являются вращением друг друга");
+            System.out.println("2.\nВведенные вами строки НЕ являются вращением друг друга");
 
         // 3
-        System.out.printf("Переворачиваем первую строку: %s\n", reverseStr(str1));
-        System.out.printf("Переворачиваем вторую строку: %s", reverseStr(str2));
+        System.out.println();
+        System.out.printf("3.\nПереворачиваем первую строку: %s\n", reverseStr(str1));
+        System.out.printf("Переворачиваем вторую строку: %s\n", reverseStr(str2));
+
+        // 4
+        System.out.println();
+        System.out.println("4.\nПримеры в виде строк:");
+        int a = 3, b = 56;
+        System.out.println(compilerMathToStr(a, b, '+'));
+        System.out.println(compilerMathToStr(a, b, '-'));
+        System.out.println(compilerMathToStr(a, b, '*'));
+    
+        // 5
+        System.out.println();
+        System.out.println("5-6.\nПримеры с заменой знака '=' на слово");
+        System.out.println(replacEqual(compilerMathToStr(a, b, '+')));
+
+        // 6
+        System.out.println(replacEqual(compilerMathToStr(a, b, '-')));
+        System.out.println();
     }
 
-    // 4 Дано два числа, например 3 и 56, необходимо составить следующие строки:
-    // 3 + 56 = 59
-    // 3 – 56 = -53
-    // 3 * 56 = 168
-    // Используем метод StringBuilder.append().
-    // 5 Замените символ “=” на слово “равно”. Используйте методы
-    // StringBuilder.insert(),
-    // StringBuilder.deleteCharAt().
-    // 6 *Замените символ “=” на слово “равно”. Используйте методы
-    // StringBuilder.replace().
     // 7 **Сравнить время выполнения пункта 6 со строкой содержащей 10000 символов
     // "="
     // средствами String и StringBuilder.
