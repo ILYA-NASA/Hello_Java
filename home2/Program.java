c60b9bde162ae179dcc1fc20152402a48251cfae
Илья Насалевич <il.nasa@outlook.com>
2022-08-14 11:55:37 +0300

    exercises 2 and 3 from homework 2 is done

--- home2/Program.java
+++ home2/Program.java
@@ -1,5 +1,6 @@
 package home2;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,23 +8,56 @@ public class Program {
     // 1 Напишите программу на Java, чтобы найти наименьшее окно в строке,
     // содержащей все символы другой строки
     public static void minWindow(String firstText, String secondText) {
-        Pattern pattern = Pattern.compile(secondText); 
-        Matcher matcher = pattern.matcher(firstText); 
+        Pattern pattern = Pattern.compile(secondText);
+        Matcher matcher = pattern.matcher(firstText);
         while (matcher.find()) {
             System.out.println(firstText.substring(matcher.start(), matcher.end()));
         }
-    //через реулярные выражения находим все полные совпадения текста второй строки с первой
-    //после семинара появилось впечатление что все окна одинаковые, а значит наименьшие 
-    }
-
-    public static void main(String[] args) {
-        // minWindow("hello world - hell words", "hell");
-        minWindow("hello world - hell words - help", "hell");
+        // через регулярные выражения находим все полные совпадения текста второй строки
+        // с первой.
+        // после семинара появилось впечатление что все окна одинаковые, а значит -
+        // наименьшие.
+        // работает на латинице
     }
 
     // 2 Напишите программу на Java, чтобы проверить, являются ли две данные строки
     // вращением друг друга.
+    public static boolean searchPalindrom(String firstStr, String secondStr) {
+        return firstStr.equals(new StringBuilder(secondStr).reverse().toString());
+    }
+    
     // 3 *Напишите программу на Java, чтобы перевернуть строку с помощью рекурсии.
+    public static String reverseStr(String str) {
+        if (str.length() <= 1) {
+           return str;
+        }
+        return reverseStr(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите первую строку: ");
+        String str1 = iScanner.nextLine();
+        System.out.printf("Введите вторую строку: ");
+        String str2 = iScanner.nextLine();
+        iScanner.close();
+
+        // 1
+        System.out.println("Ниже все наименьшие окна первой строки, содержащие все символы второй строки");
+        minWindow(str1, str2);
+        System.out.println();
+
+        // 2
+        if (searchPalindrom(str1, str2)) {
+            System.out.println("Введенные вами строки являются вращением друг друга");
+        } else
+            System.out.println("Введенные вами строки НЕ являются вращением друг друга");
+
+        // 3
+        System.out.printf("Переворачиваем первую строку: %s\n", reverseStr(str1));
+        System.out.printf("Переворачиваем вторую строку: %s", reverseStr(str2));
+    }
+
     // 4 Дано два числа, например 3 и 56, необходимо составить следующие строки:
     // 3 + 56 = 59
     // 3 – 56 = -53
