62e1a07919f77cacb5b4a7c3eb4f4b6bd3947bef
Илья Насалевич <il.nasa@outlook.com>
2022-08-19 17:23:56 +0300

    exercise 1 from homework 4 is done

--- home4/Program.java
+++ home4/Program.java
@@ -1,8 +1,27 @@
 package home4;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Program {
     // 1. Объявить и инициализировать связанный список в 10000 элементов случайным
     // числом от -1024 до 1024
+    public static List randomLinkedList(int max, int min) {
+        List<Integer> randList = new LinkedList<>();
+        while (randList.size() <= 10_000) {
+            randList.add((int) (Math.random() * ((max - min) + 1)) + min);
+        }
+        return randList;
+    }
+    
+    public static void main(String[] args) {
+        // 1
+        int max = 1024, min = -1024;
+        System.out.println(randomLinkedList(max, min));
+    }
+
+
+
     // 2. Пройти по списку и удалить все отрицательные элементы
     // 3. Скопировать в переменные и удалить из списка первый и второй элементы,
     // сложить переменные, результат сохранить в начало списка.
