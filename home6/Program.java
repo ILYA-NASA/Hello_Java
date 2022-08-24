dbd923312122e6862aa9b290a6f9539a501c835d
Илья Насалевич <il.nasa@outlook.com>
2022-08-24 21:57:39 +0300

    ex 5 is done

--- home6/Program.java
+++ home6/Program.java
@@ -1,6 +1,7 @@
 package home6;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -11,31 +12,42 @@ public class Program {
 
     public static void main(String[] args) {
         // 1.
-        System.out.println("1. Объявить и инициализировать множества HashSet hs, LinkedHashSet lhs и TreeSet ts:");
+        System.out.println("\n1. Объявить и инициализировать множества HashSet hs, LinkedHashSet lhs и TreeSet ts:");
         Set<Integer> hs = new HashSet<>();
         Set<Integer> lhs = new LinkedHashSet<>();
         Set<Integer> ts = new TreeSet<>();
         System.out.println(hs + " - HashSet (hs)\n" + lhs + " - LinkedHashSet (lhs)\n" + ts + " - TreeSet (ts)");
         // 2.
         System.out.println("\n2. Добавить в множества по 10 случайных целочисленных ключей:");
-        hs.addAll(rndmList());
-        lhs.addAll(rndmList());
-        ts.addAll(rndmList());
+        hs.addAll(rndmList(10));
+        lhs.addAll(rndmList(10));
+        ts.addAll(rndmList(10));
         System.out.println("hs: " + hs + "\nlhs: " + lhs + "\nts: " + ts);
         // 3.
         System.out.println(
                 "\n3. Пройти по множеству hs и, при условии наличия соответствующего ключа в множестве lhs, удалить ключ из hs:");
         System.out.println("hs: " + removeElementSet(hs, lhs));
-        // 3.
+        // 4.
         System.out.println(
                 "\n4. Пройти по множеству lhs и, при условии отсутствия соответствующего ключа в множестве ts, добавит ключ в ts:");
         System.out.println("lhs: " + addElementSet(lhs, ts));
+        // 5.
+        System.out.println(
+                "\n5. Объявить и инициализировать множество TreeSet ts1 с компаратором Integer (-1 при значении %2==0 ;0 при ==;1 при значении %2!=0). Заполнить ts1 15 случайными числами и вывести на печать:");
+        Set<Integer> ts1 = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer int1, Integer int2) {
+                return int1 == int2 ? 0 : int2 % 2 == 0 ? -1 : 1;
+            }
+        });
+        ts1.addAll(rndmList(15));
+        System.out.println("ts1: " + ts1 + "\n");
     }
 
     // 2.
-    private static List<Integer> rndmList() {
+    private static List<Integer> rndmList(int sizeList) {
         List<Integer> rndList = new ArrayList<>();
-        while (rndList.size() < 10) {
+        while (rndList.size() < sizeList) {
             rndList.add((int) (Math.random() * 100));
         }
         return rndList;
@@ -53,9 +65,6 @@ public class Program {
         return lhs;
     }
 
-    // 5. Объявить и инициализировать множество TreeSet ts1 с компаратором Integer
-    // (-1 при значении %2==0 ;0 при ==;1 при значении %2!=0). Заполнить ts1
-    // 15 случайными числами и вывести на печать.
     // 6. *Объявить и инициализировать множество TreeMap tm с компаратором Integer
     // (-1 при значении %2==0 ;0 при ==;1 при значении %2!=0). Заполнить
     // tm 15 случайными числами и вывести на печать.
