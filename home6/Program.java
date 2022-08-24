191f469b932fe10544fbb7fd40a6e71ad7ee361b
Илья Насалевич <il.nasa@outlook.com>
2022-08-24 11:55:22 +0300

    ex 1 and 2 is done (problem for Math.random if lhs and ts)

--- home6/Program.java
+++ home6/Program.java
@@ -1,9 +1,36 @@
 package home6;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Program {
-    // 1. Объявить и инициализировать множества HashSet hs, LinkedHashSet lhs и
-    // TreeSet ts
-    // 2. Добавить в множества по 10 случайных целочисленных ключей.
+    public static void main(String[] args) {
+        // 1. Объявить и инициализировать множества HashSet hs, LinkedHashSet lhs и
+        // TreeSet ts
+        Set<Integer> hs = new HashSet<>();
+        Set<Integer> lhs = new LinkedHashSet<>();
+        Set<Integer> ts = new TreeSet<>();
+        System.out.println(hs + " - HashSet (hs)\n" + lhs + " - LinkedHashSet (lhs)\n" + ts + " - TreeSet (ts)");
+
+        // 2. Добавить в множества по 10 случайных целочисленных ключей.
+        while (hs.size() < 10) {
+            hs.add((int) (Math.random() * 100));
+        }
+        System.out.println(hs);
+
+        // while (lhs.size() < 10) {
+        //     hs.add((int) (Math.random() * 100));
+        // }
+        // System.out.println(lhs);
+
+        // while (ts.size() < 10) {
+        //     hs.add((int) (Math.random() * 100));
+        // }
+        // System.out.println(ts);
+    }
+
     // 3. Пройти по множеству hs и, при условии наличия соответствующего ключа в
     // множестве lhs, удалить ключ из hs
     // 4. Пройти по множеству lhs и, при условии отсутствия соответствующего ключа в
